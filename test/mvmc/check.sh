852231a78e847c4eba56382e5ce4db0857b788c0
T.Aoyama <aoym@issp.u-tokyo.ac.jp>
2026-04-30 12:36:13 +0900

    update test scripts

--- test/mvmc/check.sh
+++ test/mvmc/check.sh
@@ -9,7 +9,7 @@ if [ "$mode" = "base" ]; then
 	 "$test_item" "$base_dir" "StdFace.def" "../../../src/mvmc_dry.out"
 elif [ "$mode" = "python" ]; then
     exec "${base_dir}/../common/check_case.sh" \
-	 "$test_item" "$base_dir" "StdFace.def" "stdface --solver mVMC"
+	 "$test_item" "$base_dir" "StdFace.def" "python3 -mstdface --solver mVMC"
 else
     /usr/bin/false
 fi
